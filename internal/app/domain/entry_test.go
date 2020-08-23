8dd5e1aab7491791c8ecf7f0453e1cf5464a8842
Gleb Dubrovin <termiusone@gmail.com>
2020-08-23 12:12:46 +0300

    fix test

--- internal/app/domain/entry_test.go
+++ internal/app/domain/entry_test.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -21,7 +22,7 @@ func TestEntry_UnmarshalJSON(t *testing.T) {
 			wantErr: false,
 			expectedRes: &Entry{
 				Time:   time.Unix(0, 1595768727429286952),
-				Params: []byte(`{"time":"2020-07-26T16:05:27.429286952+03:00"}`),
+				Params: []byte(fmt.Sprintf(`{"time":"%s"}`, time.Unix(0, 1595768727429286952).Format(time.RFC3339Nano))),
 			},
 		},
 		{
